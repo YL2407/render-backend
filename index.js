26fe13693085430a3d7877edf03c406a2ec897d5
Younes Laribi <44980261+YL2407@users.noreply.github.com>
2025-04-21 20:04:04 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+require('dotenv').config()
 const express = require('express')
+const Note = require('./models/note')
 
 const app = express()
 
@@ -33,24 +35,25 @@ let notes = [
   
   app.use(requestLogger);
   
-  
 
   app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
   
   app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes=>{
+      response.json(notes);
+    })
   })
 
-  app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id;
-    const note = notes.find(n=>n.id===id);
-    if(note){
-        response.json(note);
-    }else{
-        response.status(404).end();
-    }
+  app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id).then(note=>{
+      if(note){
+        response.json(note)
+      }else{
+        response.status(404).end()
+      }
+    }).catch(error=>next(error))
   })
 
   app.delete('/api/notes/:id', (request, response) => {
@@ -71,21 +74,35 @@ let notes = [
 
     const body = request.body;
 
-    if(!body.content){
-        return response.status(400).json({
-            error:'content missing'
-        })
-    }
-
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        id: generateId()
-    }
+    })
 
-    notes = notes.concat(note)
-    
-    response.json(note);
+    note.save().then(savedNote=>{
+      response.json(savedNote)
+    }).catch(error=>next(error))
+  })
+
+  app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id).then(result=>{
+      response.status(204).end()
+    }).catch(error=>next(error))
+  })
+
+  app.put('/api/notes/:id', (request, response, next) => {
+    const {content, important} = request.body
+    Note.findById(request.params.id).then(note=>{
+      if(!note){
+        return response.status(404).end()
+      }
+      note.content = content;
+      note.important=important;
+
+      return note.save().then((updatedNote)=>{
+        response.json(updatedNote)
+      })
+    }).catch(error=>next(error))
   })
 
   const unknownEndpoint = (req, res) => {
@@ -93,8 +110,21 @@ let notes = [
   }
   
   app.use(unknownEndpoint);
+
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if(error.name==='CastError'){
+      return res.status(400).send({error: 'malformatted id'})
+    }else if(error.name==='ValidationError'){
+      return response.status(400).json({error: error.message})
+    }
+
+    next(errorHandler)
+  }
   
-  const PORT = process.env.PORT || 3001
+  
+  const PORT = process.env.PORT
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
